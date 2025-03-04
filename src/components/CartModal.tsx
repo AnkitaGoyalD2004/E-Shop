054cbf7a3ff85291aaf7a79debcdcfd6cf935209
Ankita Goyal <goyalankita0325@gmail.com>
2025-03-04 22:45:01 +0530

    update

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,9 +1,43 @@
 "use client"
 
+import Image from "next/image";
+
 const CartModal = () => {
-  return (
-    <div>CartModal</div>
-  )
+
+    const cartItems = true;
+
+    return (
+        <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+            {!cartItems ? (
+                <div className="">Cart is Empty</div>
+            ) : (
+                <div className="">
+                    <Image src="https://images.pexels.com/photos/30739083/pexels-photo-30739083/free-photo-of-elegantly-plated-red-velvet-cake-slices.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load" 
+                    alt="" 
+                    width={72} 
+                    height={96} 
+                    className="object-cover rounded-md" />
+
+                <div className="" >
+                    {/* TOP */}
+                    <div className="">
+                    {/* TITLE */}
+                    <div className="">
+                        <h3>Product Name</h3>
+                        <div className="">$49</div>
+                    </div>
+                    {/* DESC */}
+                    <div className="" >
+                        available
+                    </div>
+                    {/* BOTTOM */}
+                    </div>
+                </div>
+
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default CartModal
--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import CartModal from "./CartModal";
+
 const NavIcons = () => {
     const [isProfileOpen, setIsProfileOpen] = useState(false);
   const [isCartOpen, setIsCartOpen] = useState(false);
@@ -63,6 +64,7 @@ const NavIcons = () => {
           >
             <Image src="/cart.png" alt="" width={22} height={22} />
             <div className="absolute -top-4 -right-4 w-6 h-6 bg-lama rounded-full text-white text-sm flex items-center justify-center">
+                2
             </div>
           </div>
           {isCartOpen && <CartModal />}
