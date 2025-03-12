b6160cd5126b322aa29948665babe1280960e708
Ankita Goyal <goyalankita0325@gmail.com>
2025-03-12 14:40:02 +0530

    done

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -10,6 +10,9 @@ const CartModal = () => {
             {!cartItems ? (
                 <div className="">Cart is Empty</div>
             ) : (
+                <>
+                <h2 className="text-xl">Shopping Cart</h2>
+               {/* LISt */}
                 <div className=" flex flex-col gap-8">
                     {/* ITEM */}
                 <div className="flex gap-4">
@@ -67,9 +70,24 @@ const CartModal = () => {
                 </div>
                 </div>
                 </div>
-                
+
                 </div>
-            )}
+                {/* BUTTON */}
+                <div className="">
+                    <div className="flex items-center justify-between font-semibold ">
+                        <span className="">Subtotal</span>
+                        <span className="">$49</span>
+                    </div>
+                    <p className="text-gray-500 text-sm mt-2 mb-4">
+                        Lorem ipsum dolor sit amet consectetur 
+                    </p>
+                    <div className="flex justify-between text-sm">
+                        <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">View Cart</button>
+                        <button className="rounded-md py-3 px-4 bg-black text-white">Checkout</button>
+                    </div>
+                </div>
+
+                </> )}
         </div>
     )
 }
