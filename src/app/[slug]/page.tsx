e90e989c5cca73f1a17b6eff3908fc9d4d90f27e
Ankita Goyal <goyalankita0325@gmail.com>
2025-03-14 18:36:52 +0530

    update

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Add from "@/components/Add";
+import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
 
 const SinglePage = () => {
@@ -5,10 +7,48 @@ const SinglePage = () => {
     <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-       <ProductImages/>
+        <ProductImages />
       </div>
       {/* TEXTS */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-6">Texts</div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde corporis nisi repellendus atque perspiciatis reiciendis
+           officiis natus fuga quibusdam? Tenetur impedit maxime odit voluptatum possimus perferendis nemo, quasi laborum autem.
+        </p>
+        <div className="h-[2px] bg-gray-100"/>
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">$59</h3>
+          <h2 className="font-medium text-2xl">$48</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100"/>
+        <CustomizeProducts/>
+        <Add/>
+        <div className="h-[2px] bg-gray-100"/>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ratione praesentium, iusto dolorum consectetur
+            dolores nobis cumque voluptatem est repellendus rerum facilis voluptates voluptas, dolorem minus eveniet esse qui.
+          </p>
+        </div>
+
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ratione praesentium, iusto dolorum consectetur
+            dolores nobis cumque voluptatem est repellendus rerum facilis voluptates voluptas, dolorem minus eveniet esse qui.
+          </p>
+        </div>
+
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ratione praesentium, iusto dolorum consectetur
+            dolores nobis cumque voluptatem est repellendus rerum facilis voluptates voluptas, dolorem minus eveniet esse qui.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
