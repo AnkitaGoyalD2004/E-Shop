3de27d75a4988bc7060d3d8efdd3717ba93c9317
Ankita Goyal <goyalankita0325@gmail.com>
2025-03-16 15:57:36 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { WixClientContextProvider } from "@/context/wixContext";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -20,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <WixClientContextProvider>
         <Navbar/>
         {children}
         <Footer/>
+        </WixClientContextProvider>
         </body>
     </html>
   );
--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -59,8 +59,8 @@ const NavIcons = () => {
             className="cursor-pointer"
           />
           <div
-            className="relative cursor-pointer"
-            onClick={() => setIsCartOpen((prev) => !prev)}
+            className="relative cursor-pointer"  onClick={() => setIsCartOpen((prev) => !prev)}
+           
           >
             <Image src="/cart.png" alt="" width={22} height={22} />
             <div className="absolute -top-4 -right-4 w-6 h-6 bg-lama rounded-full text-white text-sm flex items-center justify-center">
